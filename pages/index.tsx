a1e7c6b8a6cb813562e8c599b0b7db9be2b30cb9
Valentino Guerra <valenwerra@gmail.com>
2022-07-08 19:54:47 -0300

    init

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { Home } from './Home';
+
+const App: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Comsa</title>
+        <meta
+          name="description"
+          content="Companía Marplatense de Saneamiento. Control de pestes: Desratización, desinsectación y desinfección."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
