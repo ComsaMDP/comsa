ee5c5c8da47cdc85f9407a632557bf4962c2c725
Valentino Guerra <valenwerra@gmail.com>
2022-08-22 10:46:49 -0300

    WhatsApp API with new number and key

--- app/constants.ts
+++ app/constants.ts
@@ -26,6 +26,6 @@ export const VALENTINO_GUERRA_URL =
 
 export const WHATSAPP_API_URL = "https://api.callmebot.com/whatsapp.php";
 
-export const WHATSAPP_API_KEY = "249232";
+export const WHATSAPP_API_KEY = "328366";
 
-export const WHATSAPP_API_PHONE = "+5492236355257";
+export const WHATSAPP_API_PHONE = "+5492235247784";
