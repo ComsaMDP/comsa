116a6d791a133c66e1af13008d62e93981c55558
Valentino Guerra <valenwerra@gmail.com>
2022-08-29 17:51:54 -0300

    Google Analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Script from "next/script";
 import Home from "../components/Home";
 import { ServicesProvider } from "../hooks/useServices";
 
@@ -14,6 +15,19 @@ const App: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-RRYZJZ0E7G"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-RRYZJZ0E7G');
+        `}
+      </Script>
       <ServicesProvider>
         <Home />
       </ServicesProvider>
