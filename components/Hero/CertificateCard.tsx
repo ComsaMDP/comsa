14e0045c66eadd3a85b23923007ff0b978017f92
Valentino Guerra <valenwerra@gmail.com>
2022-10-31 12:14:49 -0300

    Added certificicate from Ministerio de salud

--- components/Hero/CertificateCard.tsx
+++ components/Hero/CertificateCard.tsx
@@ -13,16 +13,21 @@ export const CertificateCard = () => {
       <div className="absolute left-0 top-0 h-full w-1 self-stretch rounded bg-primary lg:hidden" />
       <span className="flex-1 text-base font-light text-white md:text-xl">
         <span className="hidden lg:inline">
-          Empresa <B>habilitada</B> y <B>autorizada</B> por la Provincia de
-          Buenos Aires y por la Municipalidad de General Pueyrredon. Se{" "}
-          <B>emiten certificados</B> por cada trabajo realizado, lo que permite
-          dar cumplimiento a las disposiciones legales vigentes en la materia.
+          Empresa <B>habilitada</B> y <B>autorizada</B> por el Ministerio de
+          Salud de La Nación, la Provincia de Buenos Aires y por la
+          Municipalidad de General Pueyrredon. Se <B>emiten certificados</B> por
+          cada trabajo realizado, lo que permite dar cumplimiento a las
+          disposiciones legales vigentes en la materia.
         </span>
         <span className="lg:hidden">
-          Empresa <B>habilitada</B> y <B>autorizada</B> por PBA y por la MGP.{" "}
-          <B>Se emiten certificados</B> por trabajo realizado.
+          Empresa <B>habilitada</B> y <B>autorizada</B> por el MSAL, la PBA y la
+          MGP. <B>Se emiten certificados</B> por trabajo realizado.
         </span>
       </span>
     </DarkCard>
   );
 };
+
+/* 
+Empresa habilitada y autorizada por el Ministerio de Salud de La Nación, la Provincia de Buenos Aires y por la Municipalidad de General Pueyrredon
+*/
